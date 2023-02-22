484f6da790e3fc1cb70c5e880ef80e88acd81424
wong2 <wonderfuly@gmail.com>
2023-02-22 14:47:35 +0800

    Add logo

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,9 +1,10 @@
+import logo from '~/assets/logo.svg'
 import { Link } from '@tanstack/react-router'
 
 function Sidebar() {
   return (
     <aside className="bg-gray-900 p-5 flex flex-col text-white">
-      <span className="font-bold">ChatHub</span>
+      <img src={logo} className="w-2/3" />
       <div className="mt-10 flex flex-col gap-3">
         <span>
           <Link to="/">All-in-One</Link>
