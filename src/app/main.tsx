6b6b0f8ec0cb5877edf8b9c918bf6b886c5b3526
wong2 <wonderfuly@gmail.com>
2023-02-22 15:28:54 +0800

    Add plausible analytics

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,3 +1,4 @@
+import Plausible from 'plausible-tracker'
 import { RouterProvider } from '@tanstack/react-router'
 import { createRoot } from 'react-dom/client'
 import '../base.css'
@@ -6,3 +7,6 @@ import { router } from './router'
 const container = document.getElementById('app')!
 const root = createRoot(container)
 root.render(<RouterProvider router={router} />)
+
+const plausible = Plausible({ domain: 'chathub.gg', hashMode: true })
+plausible.enableAutoPageviews()
