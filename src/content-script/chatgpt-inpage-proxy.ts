07396dc72f6117fc53addb039d7ef27338caee41
wong2 <wonderfuly@gmail.com>
2023-03-01 11:24:12 +0800

    Fix type

--- src/content-script/chatgpt-inpage-proxy.ts
+++ src/content-script/chatgpt-inpage-proxy.ts
@@ -2,7 +2,7 @@ import Browser from 'webextension-polyfill'
 import { setupProxyExecutor } from '~services/proxy-fetch'
 
 async function main() {
-  if (window.__NEXT_DATA__) {
+  if ((window as any).__NEXT_DATA__) {
     await Browser.runtime.sendMessage({ event: 'PROXY_TAB_READY' })
     alert('Please keep this tab open, now you can go back to ChatHub')
   }
