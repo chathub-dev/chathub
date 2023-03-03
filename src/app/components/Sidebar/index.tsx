0a77ae57cc03c3901a470d42a422be65a0a0185c
wong2 <wonderfuly@gmail.com>
2023-03-03 19:46:59 +0800

    Update sidebar text

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -23,7 +23,7 @@ function Sidebar() {
     <aside className="flex flex-col pr-5">
       <img src={logo} className="w-[79px] mb-[55px] mt-[66px] ml-5" />
       <div className="flex flex-col gap-3">
-        <NavLink to="/" text="All in one" />
+        <NavLink to="/" text="All-In-One" />
         <NavLink to="/chat/$botId" params={{ botId: 'chatgpt' }} text="ChatGPT" />
         <NavLink to="/chat/$botId" params={{ botId: 'bing' }} text="Bing" />
       </div>
