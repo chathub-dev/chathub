52eea82fe5549f03e3fd4121fd03fa26d943afbf
wong2 <wonderfuly@gmail.com>
2023-03-03 15:48:50 +0800

    Move NavLink to component

---
+++ src/app/components/Sidebar/NavLink.tsx
@@ -0,0 +1,22 @@
+import { Link, LinkPropsOptions } from '@tanstack/react-router'
+
+function NavLink(props: LinkPropsOptions & { text: string }) {
+  const { text, ...linkProps } = props
+  return (
+    <Link
+      className="rounded-[10px] w-full h-[50px] pl-5 flex flex-col justify-center"
+      activeOptions={{ exact: true }}
+      activeProps={{
+        className: 'bg-[#4987FC]',
+      }}
+      inactiveProps={{
+        className: 'bg-[#F2F2F2] bg-opacity-20',
+      }}
+      {...linkProps}
+    >
+      <span className="text-white font-medium text-sm">{text}</span>
+    </Link>
+  )
+}
+
+export default NavLink
