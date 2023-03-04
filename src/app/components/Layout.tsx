25be630ff5b00b3e5ed6a4fef6aeade21144d8d1
wong2 <wonderfuly@gmail.com>
2023-03-04 20:35:38 +0800

    Decrease verticle padding

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar'
 
 function Layout() {
   return (
-    <div className="bg-[#6756BD] h-screen py-14 px-5">
+    <div className="bg-[#6756BD] h-screen py-6 px-5">
       <main className="grid grid-cols-[20%_1fr] h-full bg-[#ffffff66] rounded-[50px] max-w-[1316px] mx-auto backdrop-blur-2xl pl-5 py-4 pr-4">
         <Sidebar />
         <Outlet />
