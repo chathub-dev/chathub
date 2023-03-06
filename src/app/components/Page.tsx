2aa755202c978abeafb617901e5ba0f0b5452dc7
wong2 <wonderfuly@gmail.com>
2023-03-06 12:30:02 +0800

    Update PagePanel radius

--- src/app/components/Page.tsx
+++ src/app/components/Page.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react'
 
 const PagePanel: FC<PropsWithChildren<{ title: string }>> = (props) => {
   return (
-    <div className="flex flex-col overflow-hidden bg-white rounded-[60px] h-full">
+    <div className="flex flex-col overflow-hidden bg-white rounded-[35px] h-full">
       <div className="text-center border-b border-solid border-[rgb(237,237,237)] h-[80px] flex flex-col justify-center mx-10">
         <span className="font-semibold text-[#303030] text-lg">{props.title}</span>
       </div>
