70bce44befb6a0dc7ce21d5954cead41821ebfe3
wong2 <wonderfuly@gmail.com>
2023-03-07 16:52:31 +0800

    Tweak markdown style

--- src/app/components/Chat/MessageBubble.tsx
+++ src/app/components/Chat/MessageBubble.tsx
@@ -10,7 +10,7 @@ const MessageBubble: FC<PropsWithChildren<Props>> = (props) => {
   return (
     <div
       className={cx(
-        'rounded-[20px] px-4 py-3',
+        'rounded-[20px] px-4 py-2',
         props.color === 'primary' ? 'bg-[#4987FC] text-white' : 'bg-[#F2F2F2] text-[#303030]',
         props.className,
       )}
