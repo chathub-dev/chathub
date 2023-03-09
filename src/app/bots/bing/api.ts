dfc9a6bd99281ffbc6e17ad3f19d98347e26f373
wong2 <wonderfuly@gmail.com>
2023-03-09 12:43:51 +0800

    Replace wretch with ofetch

--- src/app/bots/bing/api.ts
+++ src/app/bots/bing/api.ts
@@ -1,16 +1,15 @@
-import wretch from 'wretch'
+import { ofetch } from 'ofetch'
 import { uuid } from '~utils'
 import { ChatError, ErrorCode } from '~utils/errors'
 import { ConversationResponse } from './types'
 
 export async function createConversation(): Promise<ConversationResponse> {
-  const resp: ConversationResponse = await wretch('https://www.bing.com/turing/conversation/create')
-    .headers({
+  const resp = await ofetch<ConversationResponse>('https://www.bing.com/turing/conversation/create', {
+    headers: {
       'x-ms-client-request-id': uuid(),
       'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.10.0 OS/Win32',
-    })
-    .get()
-    .json()
+    },
+  })
   if (resp.result.value !== 'Success') {
     const message = `${resp.result.value}: ${resp.result.message}`
     if (resp.result.value === 'UnauthorizedRequest') {
