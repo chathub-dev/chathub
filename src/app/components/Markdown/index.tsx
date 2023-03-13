fc259487d77e5692efdb903ff666f5580a6b59ce
wong2 <wonderfuly@gmail.com>
2023-03-13 10:43:30 +0800

    Add copy code button

--- src/app/components/Markdown/index.tsx
+++ src/app/components/Markdown/index.tsx
@@ -1,13 +1,45 @@
 /* eslint-disable react/prop-types */
+import reactNodeToString from 'react-node-to-string'
+import cx from 'classnames'
 import 'github-markdown-css/github-markdown-light.css'
 import 'highlight.js/styles/github.css'
-import { FC } from 'react'
+import { FC, ReactNode, useEffect, useMemo, useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { BsClipboard } from 'react-icons/bs'
+import { FiClipboard } from 'react-icons/fi'
+import { BiClipboard } from 'react-icons/bi'
 import ReactMarkdown from 'react-markdown'
 import rehypeHighlight from 'rehype-highlight'
 import remarkGfm from 'remark-gfm'
 import supersub from 'remark-supersub'
 import Tooltip from '../Tooltip'
 import './markdown.css'
+
+function CustomCode(props: { children: ReactNode; className?: string }) {
+  const [copied, setCopied] = useState(false)
+
+  const code = useMemo(() => reactNodeToString(props.children), [props.children])
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => setCopied(false), 1000)
+    }
+  }, [copied])
+
+  return (
+    <div className="flex flex-col">
+      <div className="bg-[#e6e7e8] text-xs p-2">
+        <CopyToClipboard text={code} onCopy={() => setCopied(true)}>
+          <div className="flex flex-row items-center gap-2 cursor-pointer w-fit ml-1">
+            <BsClipboard />
+            <span>{copied ? 'copied' : 'copy code'}</span>
+          </div>
+        </CopyToClipboard>
+      </div>
+      <code className={cx(props.className, 'px-4')}>{props.children}</code>
+    </div>
+  )
+}
 
 const Markdown: FC<{ children: string }> = ({ children }) => {
   return (
@@ -26,6 +58,17 @@ const Markdown: FC<{ children: string }> = ({ children }) => {
               <a {...props} title={undefined} />
             </Tooltip>
           )
+        },
+        code: ({ node, inline, className, children, ...props }) => {
+          if (inline) {
+            return (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+          console.log(node, props)
+          return <CustomCode className={className}>{children}</CustomCode>
         },
       }}
     >
