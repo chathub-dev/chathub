3873f8fba44d2dc0fe3d6ee42f2583502f5daf99
wong2 <wonderfuly@gmail.com>
2023-03-14 10:45:22 +0800

    Increase main window size

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar'
 
 function Layout() {
   return (
-    <div className="bg-[#6756BD] h-screen py-6 px-5">
+    <div className="bg-[#6756BD] h-screen py-3 px-3">
       <main className="grid grid-cols-[20%_1fr] h-full bg-[#ffffff66] rounded-[40px] max-w-[1316px] mx-auto backdrop-blur-2xl pl-5 py-4 pr-4">
         <Sidebar />
         <Outlet />
