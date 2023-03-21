88b52f9a76f2d22d4a52b37f6bb8e5bd84a8ee92
wong2 <wonderfuly@gmail.com>
2023-03-21 19:10:47 +0800

    Improve dialog style

---
+++ src/app/components/Dialog.tsx
@@ -0,0 +1,33 @@
+import { Dialog as HeadlessDialog } from '@headlessui/react'
+import cx from 'classnames'
+import { FC, PropsWithChildren } from 'react'
+import closeIcon from '~/assets/icons/close.svg'
+
+interface Props {
+  title: string
+  open: boolean
+  onClose: () => void
+  className?: string
+}
+
+const Dialog: FC<PropsWithChildren<Props>> = (props) => {
+  return (
+    <HeadlessDialog open={props.open} onClose={props.onClose} className="relative z-50">
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center">
+          <HeadlessDialog.Panel className={cx('mx-auto rounded-[36px] bg-white shadow-xl px-5', props.className)}>
+            <HeadlessDialog.Title className="border-b border-solid border-[#ededed] flex flex-row justify-center items-center py-4">
+              <span className="ml-auto" />
+              <span className="font-bold text-[#303030] text-base">{props.title}</span>
+              <img src={closeIcon} className="w-4 h-4 ml-auto mr-[10px] cursor-pointer" onClick={props.onClose} />
+            </HeadlessDialog.Title>
+            {props.children}
+          </HeadlessDialog.Panel>
+        </div>
+      </div>
+    </HeadlessDialog>
+  )
+}
+
+export default Dialog
--- src/app/components/PromptLibrary/Dialog.tsx
+++ src/app/components/PromptLibrary/Dialog.tsx
@@ -1,5 +1,5 @@
-import { Dialog } from '@headlessui/react'
 import PromptLibrary from './Library'
+import Dialog from '../Dialog'
 
 interface Props {
   isOpen: boolean
@@ -9,17 +9,9 @@ interface Props {
 
 const PromptLibraryDialog = (props: Props) => {
   return (
-    <Dialog open={props.isOpen} onClose={props.onClose} className="relative z-50">
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-      <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center">
-          <Dialog.Panel className="mx-auto w-[800px] rounded-[30px] bg-white shadow-xl p-10 pt-5 min-h-[400px]">
-            <div className="text-center border-b border-solid border-[rgb(237,237,237)] flex flex-col justify-center pb-3 mb-5">
-              <span className="font-semibold text-[#303030] text-lg">Prompt Library</span>
-            </div>
-            <PromptLibrary insertPrompt={props.insertPrompt} />
-          </Dialog.Panel>
-        </div>
+    <Dialog title="Prompt Library" open={props.isOpen} onClose={props.onClose} className="w-[800px] min-h-[400px]">
+      <div className="my-5">
+        <PromptLibrary insertPrompt={props.insertPrompt} />
       </div>
     </Dialog>
   )
