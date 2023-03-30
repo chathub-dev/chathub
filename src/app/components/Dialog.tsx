b549661e33bd4400099d292bf7321d5a4e00747b
wong2 <wonderfuly@gmail.com>
2023-03-30 16:40:49 +0800

    Tweak dialog border radius

--- src/app/components/Dialog.tsx
+++ src/app/components/Dialog.tsx
@@ -17,7 +17,7 @@ const Dialog: FC<PropsWithChildren<Props>> = (props) => {
       <div className="fixed inset-0 flex items-center justify-center max-h-screen m-5">
         <HeadlessDialog.Panel
           className={cx(
-            'mx-auto rounded-[36px] bg-white shadow-xl max-h-full overflow-hidden flex flex-col',
+            'mx-auto rounded-3xl bg-white shadow-xl max-h-full overflow-hidden flex flex-col',
             props.className,
           )}
         >
