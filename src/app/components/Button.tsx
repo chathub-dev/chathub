7cc7a87adb93596715b87a0aff06efc9b2f13bdb
wong2 <wonderfuly@gmail.com>
2023-03-31 14:15:14 +0800

    Add default type to button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import { ButtonHTMLAttributes, FC, ReactNode, useCallback } from 'react'
+import { ButtonHTMLAttributes, FC, ReactNode } from 'react'
 import { BeatLoader } from 'react-spinners'
 
 interface Props {
@@ -15,23 +15,16 @@ interface Props {
 
 const Button: FC<Props> = (props) => {
   const size = props.size || 'normal'
-
-  const onClick = useCallback(
-    (_e: React.MouseEvent<HTMLButtonElement>) => {
-      props.onClick?.()
-    },
-    [props],
-  )
-
+  const type = props.type || 'button'
   return (
     <button
-      type={props.type}
+      type={type}
       className={cx(
         size === 'normal' ? 'rounded-[60px] text-base font-medium px-8 py-2' : 'rounded-[30px] text-sm px-4 py-1',
         props.color === 'primary' ? 'text-white bg-[#4987FC]' : 'text-[#303030] bg-[#F2F2F2]',
         props.className,
       )}
-      onClick={onClick}
+      onClick={props.onClick}
     >
       {props.isLoading ? (
         <BeatLoader size={size === 'normal' ? 10 : 5} color={props.color === 'primary' ? 'white' : '#303030'} />
