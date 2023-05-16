d434f548c2141a6f9820021a22935cd46363d205
wong2 <wonderfuly@gmail.com>
2023-05-16 14:18:27 +0800

    Check product_id in activate process

--- src/services/lemonsqueezy.ts
+++ src/services/lemonsqueezy.ts
@@ -1,18 +1,28 @@
 import { ofetch } from 'ofetch'
 
+type ActivateResponse =
+  | {
+      activated: true
+      instance: { id: string }
+      meta: {
+        product_id: number
+      }
+    }
+  | { activated: false; error: string }
+
 async function activateLicense(key: string) {
-  const resp = await ofetch<{ activated: true; instance: { id: string } } | { activated: false; error: string }>(
-    'https://api.lemonsqueezy.com/v1/licenses/activate',
-    {
-      method: 'POST',
-      body: {
-        license_key: key,
-        instance_name: 'Instance',
-      },
+  const resp = await ofetch<ActivateResponse>('https://api.lemonsqueezy.com/v1/licenses/activate', {
+    method: 'POST',
+    body: {
+      license_key: key,
+      instance_name: 'Instance',
     },
-  )
+  })
   if (!resp.activated) {
     throw new Error(resp.error)
+  }
+  if (resp.meta.product_id !== 58153) {
+    throw new Error('Unmatching product')
   }
   return resp.instance.id
 }
