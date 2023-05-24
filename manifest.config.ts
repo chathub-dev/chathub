adec11ee2c0502532b8827fdeebb41f80bbd8e50
wong2 <wonderfuly@gmail.com>
2023-05-24 14:07:35 +0800

    Change manifest to ts

---
+++ manifest.config.ts
@@ -0,0 +1,42 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+
+export default defineManifest(async (env) => {
+  return {
+    manifest_version: 3,
+    name: '__MSG_appName__',
+    description: '__MSG_appDesc__',
+    default_locale: 'en',
+    version: '1.18.8',
+    icons: {
+      '16': 'src/assets/icon.png',
+      '32': 'src/assets/icon.png',
+      '48': 'src/assets/icon.png',
+      '128': 'src/assets/icon.png',
+    },
+    background: {
+      service_worker: 'src/background/index.ts',
+      type: 'module',
+    },
+    action: {},
+    host_permissions: ['https://*.bing.com/', 'https://*.openai.com/', 'https://bard.google.com/'],
+    optional_host_permissions: ['https://*/*'],
+    permissions: ['commands', 'storage', 'unlimitedStorage'],
+    content_scripts: [
+      {
+        matches: ['https://chat.openai.com/*'],
+        js: ['src/content-script/chatgpt-inpage-proxy.ts'],
+      },
+    ],
+    commands: {
+      'open-app': {
+        suggested_key: {
+          default: 'Alt+J',
+          windows: 'Alt+J',
+          linux: 'Alt+J',
+          mac: 'Command+J',
+        },
+        description: 'Open ChatHub app',
+      },
+    },
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import manifest from './manifest.json'
+import manifest from './manifest.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
