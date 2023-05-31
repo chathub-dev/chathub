fcea5ed33085a17dc5fb7099156ffc8999336f24
wong2 <wonderfuly@gmail.com>
2023-05-31 16:11:42 +0800

    Fix insert new line at cursor position

--- src/app/components/Chat/TextInput.tsx
+++ src/app/components/Chat/TextInput.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames'
-import React, { KeyboardEventHandler, useCallback } from 'react'
+import React, { KeyboardEventHandler, useCallback, useImperativeHandle, useRef } from 'react'
 import TextareaAutosize, { TextareaAutosizeProps } from 'react-textarea-autosize'
 
 type Props = TextareaAutosizeProps & {
@@ -8,14 +8,30 @@ type Props = TextareaAutosizeProps & {
 }
 
 const TextInput = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
-  const { className, value = '', onValueChange, minRows = 1, formref, disabled, ...textareaProps } = props
+  const {
+    className,
+    value = '',
+    onValueChange,
+    minRows = 1,
+    formref,
+    disabled,
+    ...textareaProps
+  } = props as Props & { value: string }
+
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+
+  useImperativeHandle(ref, () => inputRef.current!)
 
   const onKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = useCallback(
     (e) => {
       if (e.keyCode === 13) {
         e.preventDefault()
         if (e.shiftKey) {
-          onValueChange(value + '\n')
+          const pos = inputRef.current?.selectionStart || 0
+          onValueChange(`${value.slice(0, pos)}\n${value.slice(pos)}`)
+          setTimeout(() => {
+            inputRef.current!.setSelectionRange(pos + 1, pos + 1)
+          }, 0)
         } else if (!disabled) {
           formref?.current?.requestSubmit()
         }
@@ -26,7 +42,7 @@ const TextInput = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
 
   return (
     <TextareaAutosize
-      ref={ref}
+      ref={inputRef}
       className={cx(
         'resize-none overflow-x-hidden overflow-y-auto  w-full outline-none text-sm text-primary-text bg-transparent scrollbar-thin',
         disabled && 'cursor-wait',
