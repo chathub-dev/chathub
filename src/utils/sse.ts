175c5522b97125d826fcca3e50357f68ec7ba13c
wong2 <wonderfuly@gmail.com>
2023-06-06 17:44:24 +0800

    Improve network error

--- src/utils/sse.ts
+++ src/utils/sse.ts
@@ -1,11 +1,15 @@
 import { createParser } from 'eventsource-parser'
 import { isEmpty } from 'lodash-es'
+import { ChatError, ErrorCode } from './errors'
 import { streamAsyncIterable } from './stream-async-iterable'
 
 export async function parseSSEResponse(resp: Response, onMessage: (message: string) => void) {
   if (!resp.ok) {
     const error = await resp.json().catch(() => ({}))
-    throw new Error(!isEmpty(error) ? JSON.stringify(error) : `${resp.status} ${resp.statusText}`)
+    if (!isEmpty(error)) {
+      throw new Error(JSON.stringify(error))
+    }
+    throw new ChatError(`${resp.status} ${resp.statusText}`, ErrorCode.NETWORK_ERROR)
   }
   const parser = createParser((event) => {
     if (event.type === 'event') {
