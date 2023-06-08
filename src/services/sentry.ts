c6cb10ea8361e1c57f4c4d9bbb433faa2ade7c58
wong2 <wonderfuly@gmail.com>
2023-06-08 13:33:11 +0800

    Add ExtraErrorDataIntegration to sentry

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/react'
+import { ExtraErrorData as ExtraErrorDataIntegration } from '@sentry/integrations'
 import { getVersion, isProduction } from '../utils'
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   debug: !isProduction(),
   release: getVersion(),
+  integrations: [new ExtraErrorDataIntegration({ depth: 3 })],
   sampleRate: 1.0,
 })
 
