bc3984f2b4c5cee05d8c65f7ccee36ca4183e9bf
wong2 <wonderfuly@gmail.com>
2023-07-07 14:00:16 +0800

    Release v1.26.1

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,7 +6,7 @@ export default defineManifest(async (env) => {
     name: '__MSG_appName__',
     description: '__MSG_appDesc__',
     default_locale: 'en',
-    version: '1.26.0',
+    version: '1.26.1',
     icons: {
       '16': 'src/assets/icon.png',
       '32': 'src/assets/icon.png',
