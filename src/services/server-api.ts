e31fcc81aaf4bc5f737c51ed39f93f601e3605cd
wong2 <wonderfuly@gmail.com>
2023-08-26 11:52:37 +0800

    Fix poe decode formkey

--- src/services/server-api.ts
+++ src/services/server-api.ts
@@ -1,9 +1,9 @@
 import { ofetch } from 'ofetch'
 
-export async function decodePoeFormkey(headHtml: string): Promise<string> {
+export async function decodePoeFormkey(html: string): Promise<string> {
   const resp = await ofetch('https://chathub.gg/api/poe/decode-formkey', {
     method: 'POST',
-    body: { headHtml },
+    body: { html },
   })
   return resp.formkey
 }
