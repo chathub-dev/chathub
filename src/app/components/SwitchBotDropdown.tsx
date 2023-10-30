07f5ec1da1cc9fb02e0699f6e3d40ee8849a4ce7
wong2 <wonderfuly@gmail.com>
2023-10-30 15:48:36 +0800

    Add scroll to bot dropdown menu

--- src/app/components/SwitchBotDropdown.tsx
+++ src/app/components/SwitchBotDropdown.tsx
@@ -23,7 +23,7 @@ const SwitchBotDropdown: FC<Props> = (props) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute left-0 z-10 mt-2 rounded-md bg-secondary shadow-lg focus:outline-none">
+        <Menu.Items className="absolute left-0 z-10 mt-1 py-1 rounded-md bg-secondary shadow-lg focus:outline-none max-h-[300px] overflow-y-auto">
           {enabledBots.map(({ botId, bot }) => {
             if (botId === props.selectedBotId) {
               return null
