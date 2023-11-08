16614c70fd8802a95c1b79f6f9aa95bd44561f47
wong2 <wonderfuly@gmail.com>
2023-11-08 12:19:08 +0800

    Release v1.39.4

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,12 +1,12 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
-export default defineManifest(async (env) => {
+export default defineManifest(async () => {
   return {
     manifest_version: 3,
     name: '__MSG_appName__',
     description: '__MSG_appDesc__',
     default_locale: 'en',
-    version: '1.39.3',
+    version: '1.39.4',
     icons: {
       '16': 'src/assets/icon.png',
       '32': 'src/assets/icon.png',
