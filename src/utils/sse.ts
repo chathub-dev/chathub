da7d7132e4595f44a41fb65ca0fc4e97312bbe06
wong2 <wonderfuly@gmail.com>
2023-11-16 14:38:38 +0800

    Add fallback status text for sse error response

--- src/utils/sse.ts
+++ src/utils/sse.ts
@@ -3,13 +3,21 @@ import { isEmpty } from 'lodash-es'
 import { ChatError, ErrorCode } from './errors'
 import { streamAsyncIterable } from './stream-async-iterable'
 
+const statusTextMap = new Map([
+  [400, 'Bad Request'],
+  [401, 'Unauthorized'],
+  [403, 'Forbidden'],
+  [429, 'Too Many Requests'],
+])
+
 export async function parseSSEResponse(resp: Response, onMessage: (message: string) => void) {
   if (!resp.ok) {
     const error = await resp.json().catch(() => ({}))
     if (!isEmpty(error)) {
       throw new Error(JSON.stringify(error))
     }
-    throw new ChatError(`${resp.status} ${resp.statusText}`, ErrorCode.NETWORK_ERROR)
+    const statusText = resp.statusText || statusTextMap.get(resp.status) || ''
+    throw new ChatError(`${resp.status} ${statusText}`, ErrorCode.NETWORK_ERROR)
   }
   const parser = createParser((event) => {
     if (event.type === 'event') {
