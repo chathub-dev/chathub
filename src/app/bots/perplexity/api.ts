9a137e993a8d3123f456a0d5ffbc82b1998d9dc3
wong2 <wonderfuly@gmail.com>
2023-11-21 14:24:43 +0800

    Move initSession to a function

--- src/app/bots/perplexity/api.ts
+++ src/app/bots/perplexity/api.ts
@@ -7,14 +7,18 @@ async function getSessionId() {
   return sessionId
 }
 
-export async function createSession(): Promise<string> {
-  const sessionId = await getSessionId()
+async function initSession(sessionId: string) {
   const resp = await ofetch(`https://labs-api.perplexity.ai/socket.io/?EIO=4&transport=polling&sid=${sessionId}`, {
     method: 'POST',
     body: '40{"jwt":"anonymous-ask-user"}',
   })
   if (resp !== 'OK') {
-    throw new Error('Failed to create session')
+    throw new Error('Failed to init perplexity session')
   }
+}
+
+export async function createSession(): Promise<string> {
+  const sessionId = await getSessionId()
+  await initSession(sessionId)
   return sessionId
 }
