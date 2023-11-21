15799b5dda8cf932c1bc22226b0f3652dbd19401
wong2 <wonderfuly@gmail.com>
2023-11-21 11:37:30 +0800

    Tweak page title size

--- src/app/components/Page.tsx
+++ src/app/components/Page.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren } from 'react'
 const PagePanel: FC<PropsWithChildren<{ title: string }>> = (props) => {
   return (
     <div className="flex flex-col overflow-hidden bg-primary-background dark:text-primary-text rounded-2xl h-full">
-      <div className="text-center border-b border-solid border-primary-border flex flex-col justify-center mx-10 py-3">
+      <div className="text-center border-b border-solid border-primary-border flex flex-col justify-center mx-10 py-2">
         <span className="font-semibold text-lg">{props.title}</span>
       </div>
       <div className="px-10 h-full overflow-auto">{props.children}</div>
