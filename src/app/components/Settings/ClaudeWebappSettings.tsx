097923cfc64baf0a0d855cdf9a8ebcfb82033f70
wong2 <wonderfuly@gmail.com>
2023-11-30 16:14:23 +0800

    Add description for webapp mode

--- src/app/components/Settings/ClaudeWebappSettings.tsx
+++ src/app/components/Settings/ClaudeWebappSettings.tsx
@@ -12,6 +12,9 @@ const ClaudeWebappSettings: FC<Props> = () => {
   const { t } = useTranslation()
   return (
     <div className="flex flex-col gap-1">
+      <blockquote className="text-sm font-medium border-l-4 border-gray-300 pl-2 italic mb-1">
+        {t('Webapp mode uses your login session in current browser')}
+      </blockquote>
       <p className="font-medium text-sm">{t('Model')}</p>
       <div className="w-[250px] mb-1">
         <Select options={[{ name: 'Claude 2', value: 'claude-2' }]} value="claude-2" onChange={console.log} />
