f77e58752cadc1e921a1ad64882e8132ca9ad542
wong2 <wonderfuly@gmail.com>
2023-11-30 16:52:56 +0800

    Tweak webapp mode tip style

--- src/app/components/Settings/ClaudeWebappSettings.tsx
+++ src/app/components/Settings/ClaudeWebappSettings.tsx
@@ -12,7 +12,7 @@ const ClaudeWebappSettings: FC<Props> = () => {
   const { t } = useTranslation()
   return (
     <div className="flex flex-col gap-1">
-      <blockquote className="text-sm font-medium border-l-4 border-gray-300 pl-2 italic mb-1">
+      <blockquote className="text-sm border-l-4 border-gray-300 pl-2 italic mb-1">
         {t('Webapp mode uses your login session in current browser')}
       </blockquote>
       <p className="font-medium text-sm">{t('Model')}</p>
