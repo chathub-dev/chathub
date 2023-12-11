b60d513d44ab0ba1d33340b0ed871a32a2a8cec5
wong2 <wonderfuly@gmail.com>
2023-12-11 17:26:36 +0800

    Remove top-level await in content script

--- src/content-script/twitter.ts
+++ src/content-script/twitter.ts
@@ -3,5 +3,6 @@ import Browser from 'webextension-polyfill'
 
 const csrfToken = Cookies.get('ct0') || ''
 
-await Browser.storage.session.set({ 'twitter-csrf-token': csrfToken })
-console.debug('sync twitter csrf token', csrfToken)
+Browser.storage.session.set({ 'twitter-csrf-token': csrfToken }).then(() => {
+  console.debug('sync twitter csrf token', csrfToken)
+})
