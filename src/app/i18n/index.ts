9a3cb66b7b60949edc180bf283529473c1c00e32
バンドウイ茶 <59186289+bondICha@users.noreply.github.com>
2025-03-06 21:57:19 +0900

    Reduce supporting language

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -2,27 +2,15 @@ import i18n, { Resource } from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next, Translation } from 'react-i18next'
 import { getLanguage } from '~services/storage/language'
-import french from './locales/french.json'
-import german from './locales/german.json'
-import indonesia from './locales/indonesia.json'
 import japanese from './locales/japanese.json'
-import portuguese from './locales/portuguese.json'
 import simplifiedChinese from './locales/simplified-chinese.json'
-import spanish from './locales/spanish.json'
-import thai from './locales/thai.json'
 import traditionalChinese from './locales/traditional-chinese.json'
 import english from './locales/english.json'
 
 const resources: Resource = {
   'zh-CN': { translation: simplifiedChinese },
   'zh-TW': { translation: traditionalChinese },
-  es: { translation: spanish },
-  pt: { translation: portuguese },
   ja: { translation: japanese },
-  de: { translation: german },
-  fr: { translation: french },
-  in: { translation: indonesia },
-  th: { translation: thai },
   en: { translation: english}
 }
 
